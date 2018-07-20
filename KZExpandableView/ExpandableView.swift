9c3961dbe3313a2df26eda46f6ee6b65fbf22e9b
Karlygash Zhuginissova <karlygash9@mail.ru>
2018-07-20 19:00:42 +0600

    initial commit

---
+++ KZExpandableView/ExpandableView.swift
@@ -0,0 +1,21 @@
+//
+//  ExpandableView.swift
+//  KZExpandableView
+//
+//  Created by Karlygash Zhuginissova on 7/20/18.
+//  Copyright © 2018 Senim. All rights reserved.
+//
+
+import UIKit
+
+class ExpandableView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
